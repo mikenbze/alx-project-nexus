9a1cdd5bcbba40b89c78cc7a0bfd184c503b2861
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-09-16 17:50:10 +0300

    edit json

--- my-duka/vite.config.ts
+++ my-duka/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // relative paths so assets load correctly on Vercel
+  base: './', // ✅ Use relative paths so JS/CSS load correctly on Vercel
   build: {
     outDir: 'dist',
     emptyOutDir: true,
