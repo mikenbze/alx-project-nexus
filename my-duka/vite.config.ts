eb9b8fad389f8d48090d2cb34a007ae2a8c53f56
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-09-16 16:12:59 +0300

    Update vite.config.ts

--- my-duka/vite.config.ts
+++ my-duka/vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',   // ← MUST be relative for Vercel
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 });
