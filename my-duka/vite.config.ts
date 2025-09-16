02b9010bc7a780c24a05affbc94e2762a0f50f69
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-09-16 14:56:12 +0300

    vite.config update

--- my-duka/vite.config.ts
+++ my-duka/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',       // Ensure build output folder is 'dist'
+    outDir: 'dist',       // production folder
     emptyOutDir: true
   }
 });
